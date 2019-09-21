2743c9c996225f4968b6febee69bfe89ce973598
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2019-09-21 09:50:45 -0400

    updating public.py

--- lib/views/public.py
+++ lib/views/public.py
@@ -4,4 +4,5 @@ public_views = flask.Blueprint('public_views', __name__)
 
 @public_views.route('/')
 def home():
-    return flask.render_template('landing_page.html')
+    return flask.render_template('landing_page.html',
+                                is_logged_in=flask.session.get('username'))
