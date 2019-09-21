54948a256db8501e44bf0012a8d1ef2ba69ff311
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2019-09-21 01:57:30 -0400

    made the app.py file

---
+++ app.py
@@ -0,0 +1,23 @@
+#Backend by Patrick and Boaz wheeee!
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    pass
+
+#route for the page to draw between stars
+@app.route('/draw', methods=['GET'])
+def draw():
+    return render_template('draw.html')
+
+#route for the page to turn drawing into stars
+@app.route('/convert')
+def convert():
+    pass
+
+#route for the login page
+@app.route('/login', methods=['GET','POST'])
+def login():
+    pass
