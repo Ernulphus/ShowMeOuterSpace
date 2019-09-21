3343d68daa283fd578bf86893840f91eca23c8d8
Patrick Chan <patrickchan1999@gmail.com>
2019-09-21 08:51:23 -0400

    Add security, move some of the routes in app.py to public/security

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
 #Backend by Patrick and Boaz wheeee!
-from flask import Flask, render_template, request
+import flask
+import sys
 
 
-app = Flask(__name__)
+from lib.security import security
+from lib.views import public
 
 
-@app.route('/', methods=['GET'])
-def landing_page():
-    return render_template('landing_page.html')
+app = flask.Flask(__name__)
+app.register_blueprint(security.security)
+app.register_blueprint(public.public_views)
 
 
-#route for the login page
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
-
-#route for the register page
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    return render_template('register.html')
+try:
+    app.secret_key = sys.argv[sys.argv.index('--key') + 1]
+except ValueError:
+    app.secret_key = 'e5fce5faa2e20b203c014f358f73c48f7129084ab1643c9fa6a0f87ff7a546a2'
+    #get value via os.urandom(32).encode('hex')
 
 
 #route for the user to select the game
 @app.route('/games', methods=['GET'])
 def games():
-    return render_template('games.html')
+    return flask.render_template('games.html')
 
 
 #route for the page to draw between stars
 @app.route('/draw', methods=['GET'])
 def draw():
-    return render_template('draw.html')
+    return flask.render_template('draw.html')
 
 
 #route for the page to turn drawing into stars
